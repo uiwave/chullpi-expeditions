f2b9a9c1280a3ad37665651627db200abbd91621
uiwave <uiwavedev@gmail.com>
2026-09-09 16:42:27 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import PopularToursSection from "../components/sections/PopularToursSection";
+
+export default function Home() {
+  return (
+    <>
+      <PopularToursSection />
+    </>
+  );
+}
---
+++ src/components/sections/PopularToursSection.tsx
@@ -0,0 +1,17 @@
+export default function PopularToursSection() {
+  return (
+    <section className="w-full overflow-hidden">
+      <div>
+        <div>
+          <div>
+            <h2>TOURS POPULARES EN PERÚ</h2>
+            <p>
+              Viajes seleccionados con guías locales expertos y servicio
+              personalizado todo incluido.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
